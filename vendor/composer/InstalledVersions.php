949f850069da5e15dedfb9a17f9480ece6f8a36f
root <root@localhost.localdomain>
2026-01-23 16:01:46 +0800

    Initial commit

---
+++ vendor/composer/InstalledVersions.php
@@ -0,0 +1,687 @@
+<?php
+
+
+
+
+
+
+
+
+
+
+
+namespace Composer;
+
+use Composer\Autoload\ClassLoader;
+use Composer\Semver\VersionParser;
+
+
+
+
+
+
+
+
+class InstalledVersions
+{
+private static $installed = array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'thinkcmf/thinkcmf',
+  ),
+  'versions' => 
+  array (
+    'adbario/php-dot-notation' => 
+    array (
+      'pretty_version' => '2.5.0',
+      'version' => '2.5.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '081e2cca50c84bfeeea2e3ef9b2c8d206d80ccae',
+    ),
+    'alibabacloud/credentials' => 
+    array (
+      'pretty_version' => '1.2.3',
+      'version' => '1.2.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f6d1986e7b7be8da781d0b99f24c92d9860ba0c1',
+    ),
+    'alibabacloud/darabonba-openapi' => 
+    array (
+      'pretty_version' => '0.2.17',
+      'version' => '0.2.17.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '051cd22db3da16ab7d0110feb3fc45dd0f999f92',
+    ),
+    'alibabacloud/dysmsapi-20170525' => 
+    array (
+      'pretty_version' => '3.1.0',
+      'version' => '3.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5f779cf8d6d8807b9835bff8d058dbab6d59bbf1',
+    ),
+    'alibabacloud/endpoint-util' => 
+    array (
+      'pretty_version' => '0.1.1',
+      'version' => '0.1.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f3fe88a25d8df4faa3b0ae14ff202a9cc094e6c5',
+    ),
+    'alibabacloud/gateway-spi' => 
+    array (
+      'pretty_version' => '1.0.0',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7440f77750c329d8ab252db1d1d967314ccd1fcb',
+    ),
+    'alibabacloud/openapi-util' => 
+    array (
+      'pretty_version' => '0.2.1',
+      'version' => '0.2.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f31f7bcd835e08ca24b6b8ba33637eb4eceb093a',
+    ),
+    'alibabacloud/tea' => 
+    array (
+      'pretty_version' => '3.2.1',
+      'version' => '3.2.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1619cb96c158384f72b873e1f85de8b299c9c367',
+    ),
+    'alibabacloud/tea-utils' => 
+    array (
+      'pretty_version' => '0.2.21',
+      'version' => '0.2.21.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5039e45714c6456186d267f5d81a4b260a652495',
+    ),
+    'alibabacloud/tea-xml' => 
+    array (
+      'pretty_version' => '0.2.4',
+      'version' => '0.2.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3e0c000bf536224eebbac913c371bef174c0a16a',
+    ),
+    'chamilo/pclzip' => 
+    array (
+      'pretty_version' => 'v2.8.5',
+      'version' => '2.8.5.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'af10d07a39922b0789bf761524a22ecefc01d405',
+    ),
+    'ezyang/htmlpurifier' => 
+    array (
+      'pretty_version' => 'v4.17.0',
+      'version' => '4.17.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'bbc513d79acf6691fa9cf10f192c90dd2957f18c',
+    ),
+    'guzzlehttp/guzzle' => 
+    array (
+      'pretty_version' => '7.10.0',
+      'version' => '7.10.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b51ac707cfa420b7bfd4e4d5e510ba8008e822b4',
+    ),
+    'guzzlehttp/promises' => 
+    array (
+      'pretty_version' => '2.3.0',
+      'version' => '2.3.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '481557b130ef3790cf82b713667b43030dc9c957',
+    ),
+    'guzzlehttp/psr7' => 
+    array (
+      'pretty_version' => '2.8.0',
+      'version' => '2.8.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '21dc724a0583619cd1652f673303492272778051',
+    ),
+    'lizhichao/one-sm' => 
+    array (
+      'pretty_version' => '1.10',
+      'version' => '1.10.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '687a012a44a5bfd4d9143a0234e1060543be455a',
+    ),
+    'mindplay/annotations' => 
+    array (
+      'pretty_version' => '1.3.4',
+      'version' => '1.3.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6d5bfc47218cb013b0b3026374c6ffb0b1cad989',
+    ),
+    'obsoletepackage/phpquery' => 
+    array (
+      'pretty_version' => '1.0.2',
+      'version' => '1.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ebe03fabd3286fdad18a0148a5f23f6dcc0443fb',
+    ),
+    'pclzip/pclzip' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '^2.8',
+      ),
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.9.1',
+      'version' => '6.9.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '039de174cd9c17a8389754d3b877a2ed22743e18',
+    ),
+    'psr/container' => 
+    array (
+      'pretty_version' => '2.0.2',
+      'version' => '2.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c71ecc56dfe541dbd90c5360474fbc405f8d5963',
+    ),
+    'psr/http-client' => 
+    array (
+      'pretty_version' => '1.0.3',
+      'version' => '1.0.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'bb5906edc1c324c9a05aa0873d40117941e5fa90',
+    ),
+    'psr/http-client-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0',
+      ),
+    ),
+    'psr/http-factory' => 
+    array (
+      'pretty_version' => '1.1.0',
+      'version' => '1.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2b4765fddfe3b508ac62f829e852b1501d3f6e8a',
+    ),
+    'psr/http-factory-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0',
+      ),
+    ),
+    'psr/http-message' => 
+    array (
+      'pretty_version' => '1.1',
+      'version' => '1.1.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'cb6ce4845ce34a8ad9e68117c10ee90a29919eba',
+    ),
+    'psr/http-message-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0',
+      ),
+    ),
+    'psr/log' => 
+    array (
+      'pretty_version' => '3.0.2',
+      'version' => '3.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f16e1d5863e37f8d8c2a01719f5b34baa2b714d3',
+    ),
+    'psr/simple-cache' => 
+    array (
+      'pretty_version' => '3.0.0',
+      'version' => '3.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '764e0b3939f5ca87cb904f570ef9be2d78a07865',
+    ),
+    'ralouphie/getallheaders' => 
+    array (
+      'pretty_version' => '3.0.3',
+      'version' => '3.0.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '120b605dfeb996808c31b6477290a714d356e822',
+    ),
+    'symfony/deprecation-contracts' => 
+    array (
+      'pretty_version' => 'v3.0.2',
+      'version' => '3.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '26954b3d62a6c5fd0ea8a2a00c0353a14978d05c',
+    ),
+    'thinkcmf/cmf' => 
+    array (
+      'pretty_version' => 'v8.0.3',
+      'version' => '8.0.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '81489195003cbd0e9767662680dd4232c10fc26a',
+    ),
+    'thinkcmf/cmf-api' => 
+    array (
+      'pretty_version' => 'v8.0.6',
+      'version' => '8.0.6.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '00d4acf2a064f8a95270797d7836d08ea3e7b540',
+    ),
+    'thinkcmf/cmf-app' => 
+    array (
+      'pretty_version' => 'v8.0.2',
+      'version' => '8.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '188393a0f424d6bbbaa033d5a1067e32b467de1c',
+    ),
+    'thinkcmf/cmf-appstore' => 
+    array (
+      'pretty_version' => 'v2.0.1',
+      'version' => '2.0.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '935711d9625c0d5ed3ddf62f082782c768fa1667',
+    ),
+    'thinkcmf/cmf-captcha' => 
+    array (
+      'pretty_version' => 'v3.0.10',
+      'version' => '3.0.10.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9300381abd25f5c312b4d659fb2232e8799c938e',
+    ),
+    'thinkcmf/cmf-extend' => 
+    array (
+      'pretty_version' => 'v5.1.5',
+      'version' => '5.1.5.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'bd4c7077792089cf573d0332e9e8a73a68135aab',
+    ),
+    'thinkcmf/cmf-install' => 
+    array (
+      'pretty_version' => 'v8.0.2',
+      'version' => '8.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f1b8040f19979a8adc01b40f82758ca7d7df7142',
+    ),
+    'thinkcmf/cmf-root' => 
+    array (
+      'pretty_version' => 'v2.0.2',
+      'version' => '2.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c4b6a024ce39ff88a6ec564852eff6d0ccbcb147',
+    ),
+    'thinkcmf/thinkcmf' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'topthink/framework' => 
+    array (
+      'pretty_version' => 'v8.0.4',
+      'version' => '8.0.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '846432655a82614a874cdaaac7be6870940b14d2',
+    ),
+    'topthink/think-helper' => 
+    array (
+      'pretty_version' => 'v3.1.8',
+      'version' => '3.1.8.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '612eea76eec2a22f41b0e24be27f49454e4fd5f5',
+    ),
+    'topthink/think-orm' => 
+    array (
+      'pretty_version' => 'v3.0.27',
+      'version' => '3.0.27.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9c691f6bedc8a47103767a9a682810308c30c010',
+    ),
+    'topthink/think-trace' => 
+    array (
+      'pretty_version' => 'v1.6',
+      'version' => '1.6.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '136cd5d97e8bdb780e4b5c1637c588ed7ca3e142',
+    ),
+    'xia/migration' => 
+    array (
+      'pretty_version' => 'v6.0.3',
+      'version' => '6.0.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c6e17b0e9910baa9221bdfe33037bbc2c9bdc7b2',
+    ),
+  ),
+);
+private static $canGetVendors;
+private static $installedByVendor = array();
+
+
+
+
+
+
+
+public static function getInstalledPackages()
+{
+$packages = array();
+foreach (self::getInstalled() as $installed) {
+$packages[] = array_keys($installed['versions']);
+}
+
+if (1 === \count($packages)) {
+return $packages[0];
+}
+
+return array_keys(array_flip(\call_user_func_array('array_merge', $packages)));
+}
+
+
+
+
+
+
+
+
+
+public static function isInstalled($packageName)
+{
+foreach (self::getInstalled() as $installed) {
+if (isset($installed['versions'][$packageName])) {
+return true;
+}
+}
+
+return false;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public static function satisfies(VersionParser $parser, $packageName, $constraint)
+{
+$constraint = $parser->parseConstraints($constraint);
+$provided = $parser->parseConstraints(self::getVersionRanges($packageName));
+
+return $provided->matches($constraint);
+}
+
+
+
+
+
+
+
+
+
+
+public static function getVersionRanges($packageName)
+{
+foreach (self::getInstalled() as $installed) {
+if (!isset($installed['versions'][$packageName])) {
+continue;
+}
+
+$ranges = array();
+if (isset($installed['versions'][$packageName]['pretty_version'])) {
+$ranges[] = $installed['versions'][$packageName]['pretty_version'];
+}
+if (array_key_exists('aliases', $installed['versions'][$packageName])) {
+$ranges = array_merge($ranges, $installed['versions'][$packageName]['aliases']);
+}
+if (array_key_exists('replaced', $installed['versions'][$packageName])) {
+$ranges = array_merge($ranges, $installed['versions'][$packageName]['replaced']);
+}
+if (array_key_exists('provided', $installed['versions'][$packageName])) {
+$ranges = array_merge($ranges, $installed['versions'][$packageName]['provided']);
+}
+
+return implode(' || ', $ranges);
+}
+
+throw new \OutOfBoundsException('Package "' . $packageName . '" is not installed');
+}
+
+
+
+
+
+public static function getVersion($packageName)
+{
+foreach (self::getInstalled() as $installed) {
+if (!isset($installed['versions'][$packageName])) {
+continue;
+}
+
+if (!isset($installed['versions'][$packageName]['version'])) {
+return null;
+}
+
+return $installed['versions'][$packageName]['version'];
+}
+
+throw new \OutOfBoundsException('Package "' . $packageName . '" is not installed');
+}
+
+
+
+
+
+public static function getPrettyVersion($packageName)
+{
+foreach (self::getInstalled() as $installed) {
+if (!isset($installed['versions'][$packageName])) {
+continue;
+}
+
+if (!isset($installed['versions'][$packageName]['pretty_version'])) {
+return null;
+}
+
+return $installed['versions'][$packageName]['pretty_version'];
+}
+
+throw new \OutOfBoundsException('Package "' . $packageName . '" is not installed');
+}
+
+
+
+
+
+public static function getReference($packageName)
+{
+foreach (self::getInstalled() as $installed) {
+if (!isset($installed['versions'][$packageName])) {
+continue;
+}
+
+if (!isset($installed['versions'][$packageName]['reference'])) {
+return null;
+}
+
+return $installed['versions'][$packageName]['reference'];
+}
+
+throw new \OutOfBoundsException('Package "' . $packageName . '" is not installed');
+}
+
+
+
+
+
+public static function getRootPackage()
+{
+$installed = self::getInstalled();
+
+return $installed[0]['root'];
+}
+
+
+
+
+
+
+
+
+public static function getRawData()
+{
+@trigger_error('getRawData only returns the first dataset loaded, which may not be what you expect. Use getAllRawData() instead which returns all datasets for all autoloaders present in the process.', E_USER_DEPRECATED);
+
+return self::$installed;
+}
+
+
+
+
+
+
+
+public static function getAllRawData()
+{
+return self::getInstalled();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public static function reload($data)
+{
+self::$installed = $data;
+self::$installedByVendor = array();
+}
+
+
+
+
+
+private static function getInstalled()
+{
+if (null === self::$canGetVendors) {
+self::$canGetVendors = method_exists('Composer\Autoload\ClassLoader', 'getRegisteredLoaders');
+}
+
+$installed = array();
+
+if (self::$canGetVendors) {
+foreach (ClassLoader::getRegisteredLoaders() as $vendorDir => $loader) {
+if (isset(self::$installedByVendor[$vendorDir])) {
+$installed[] = self::$installedByVendor[$vendorDir];
+} elseif (is_file($vendorDir.'/composer/installed.php')) {
+$installed[] = self::$installedByVendor[$vendorDir] = require $vendorDir.'/composer/installed.php';
+}
+}
+}
+
+$installed[] = self::$installed;
+
+return $installed;
+}
+}
